843a6ab12f9236e3a06eba316ee1dddc5ade92e2
Bart Erkamp <b.erkamp@gmail.com>
2022-06-13 12:59:02 +0200

    Opdrachten Variables & Datatypes afgerond

---
+++ 02_Programming/Javascript/01_basics/Opdrachten/sandbox.js
@@ -0,0 +1,43 @@
+"use strict";
+
+console.log("it works");
+
+// let letOefening = "Let Tekst";
+// let letOefening = "let Tekst Twee";
+
+// console.log(letOefening);
+
+// varOefening = "var Tekst";
+// console.log(varOefening);
+
+// var varOefening = "Var Tekst Twee!";
+// console.log(varOefening);
+
+// var varOefening = "Var Tekst Drie";
+// console.log(varOefening);
+
+// let typeQuestion = function calculateSomething() {};
+// console.log(`This is a ${typeof typeQuestion} with the value ${typeQuestion}`);
+
+// let someNumber = 5;
+// let anotherNumber = "5";
+// console.log(someNumber !== anotherNumber);
+
+// let increment = 1;
+// console.log(++increment); // 1
+// console.log(increment++); // 2
+
+// let increment = 1;
+// console.log(increment++); // 1
+// console.log(++increment); // 3
+
+// let modulo = 24 / 10;
+// console.log(modulo);
+
+// let someNumber = 1;
+// someNumber = someNumber + 1;
+// console.log(someNumber); // 2
+
+let someNumber = 1;
+++someNumber;
+console.log(someNumber); // 2
