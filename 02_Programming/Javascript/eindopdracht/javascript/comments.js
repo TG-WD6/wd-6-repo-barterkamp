033c42880ddc4b7ace731d5438e56111f9dee979
Bart Erkamp <b.erkamp@gmail.com>
2022-07-12 11:02:30 +0200

    added index.html and deleted deploy folder

--- 02_Programming/Javascript/eindopdracht/javascript/comments.js
+++ 02_Programming/Javascript/eindopdracht/javascript/comments.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// const commentsDiv = document.querySelector(".comments");
 const commentsList = document.querySelector(".comments__list");
 const commentsInput = document.querySelector(".comments__txtinput");
 const commentSender = document.querySelector(".comments__sender");
@@ -19,9 +18,9 @@ let reply = false;
 commentForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const sender = commentSender.value;
-  const comment = commentsInput.value;
-  const commentsNr = ++counter;
+  let sender = commentSender.value;
+  let comment = commentsInput.value;
+  let commentsNr = ++counter;
 
   // Fallback voor als er een lege comment wordt geplaatst.
   if (comment.trim() === "") {
@@ -36,18 +35,19 @@ commentForm.addEventListener("submit", (e) => {
   commentsListItem.setAttribute("id", commentsNr);
   commentsList.appendChild(commentsListItem);
 
-  // comments container
+  // Comments container
   const commentsContainer = document.createElement("div");
   commentsContainer.classList.add("comments__container");
   commentsListItem.append(commentsContainer);
 
-  // comments avatar section with img, name and id number
+  // Comments avatar section with avatar, name of sender and id number
   const commentsProfile = document.createElement("div");
   commentsProfile.classList.add("comments__profile");
   commentsContainer.appendChild(commentsProfile);
 
   const commentsAvatar = document.createElement("img");
   commentsAvatar.src = "../images/Pokemon-images/1.png";
+  commentsAvatar.classList.add("comments__avatar");
 
   const commentsName = document.createElement("p");
   commentsName.classList.add("comments__name");
@@ -59,26 +59,25 @@ commentForm.addEventListener("submit", (e) => {
 
   commentsProfile.append(commentsAvatar, commentsName, commentsId);
 
-  // comments content section
+  // Comments content section
   const commentsContent = document.createElement("div");
   commentsContent.classList.add("comments__content");
   commentsContainer.appendChild(commentsContent);
 
-  // link
-  const commentsLink = document.createElement("a");
-  const textNode = document.createTextNode(
-    `Replying to ${commentsName.textContent} ${commentsId.textContent}`
-  );
-  commentsLink.appendChild(textNode);
-  commentsLink.href = `#${commentsNr}`;
-  commentsLink.classList.add("comments__link");
-  commentsContent.appendChild(commentsLink);
-
   // Laat de link zien in de reply comment en maakt de replycontainer weer onzichtbaar.
 
   if (reply) {
-    commentsLink.classList.add("show-link");
+    const commentsLink = document.createElement("a");
+    const textNode = document.createTextNode(replyInformation.textContent);
+    commentsLink.appendChild(textNode);
+    commentsLink.href = `#${replyInformation.textContent.charAt(
+      replyInformation.textContent.length - 1
+    )}`;
+    commentsLink.classList.add("comments__link");
+    commentsContent.appendChild(commentsLink);
+
     replyContainer.classList.remove("show");
+
     reply = false;
   }
 
@@ -108,38 +107,8 @@ commentForm.addEventListener("submit", (e) => {
     return reply;
   });
 
-  // code voor de het verbergen van de replycontainer
+  // Code voor de het verbergen van de replycontainer
   closeBtn.addEventListener("click", () => {
     replyContainer.classList.remove("show");
   });
 });
-
-// voeg onzichtbare link toe naar comment nadat er op reply is geklikt
-
-/* <a href="`#${commentsNr}`">
-  `Replying to ${commentsName.textContent} ${commentsId.textContent}`
-</a>; */
-
-// const html = `
-// <div class="comments__container">
-//   <div class="comments__profile">
-//     <img class="comments__avatar" src="../images/Pokemon-images/1.png" alt="commenter avatar"/>
-//     <p class="comments__commenter">${sender}</p>
-//     <p class="comments__number" id="${commentsNr}">#${commentsNr}</p>
-//   </div>
-//   <div class="comments__content">
-//     <p>${comment}</p>
-//   </div>
-//   <button class="comments__reply-btn">Reply</button>
-// </div>
-// `;
-
-// commentDiv.insertAdjacentHTML("afterbegin", html);
-
-// const commentsContainer = document.querySelector(".comments__container");
-// const commenter = document.querySelector(".comments__commenter");
-// const commentsNumber = document.querySelector(".comments__number");
-
-// add link to content which scrolls to specific comment using the specific ${commentId} as anchor
-
-/* <a href="#${commentId}">link</a>; */
