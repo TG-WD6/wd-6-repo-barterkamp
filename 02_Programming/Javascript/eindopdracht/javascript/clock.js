608c141538a09687df8f6d1a2afe45f13f1da347
Bart Erkamp <b.erkamp@gmail.com>
2022-07-13 09:46:24 +0200

    scss files opgeschoond

--- 02_Programming/Javascript/eindopdracht/javascript/clock.js
+++ 02_Programming/Javascript/eindopdracht/javascript/clock.js
@@ -20,18 +20,19 @@ const seconds = document.querySelector(".second");
 const minutes = document.querySelector(".minute");
 const hours = document.querySelector(".hour");
 
-console.log();
+setInterval(clock, 1000);
 
-const clock = function () {
+function clock() {
   // let op! de today variable kan niet buiten de functie geplaatst worden, omdat je deze bij iedere functie call wilt genereren en niet enkel bij het laden van de pagina.
+
   const timeZoneOffset = document.getElementById("timezone");
 
   const offset = +timeZoneOffset.value / 10;
 
-  // values in de select zijn lelijk maar kloppen wel
+  // values in de select van HTML zijn lelijk maar kloppen wel
   // onze offset is new Date().getTimezoneOffset()/60;
   // een value van 0 in HTML is dus in werkelijkheid een offset van 2
-  // is niet schaalbaar met zomertijd, heb de juiste manier van timezone implementatie nog niet kunnen vinden.
+  // tijdzones werken maar er is vast een betere optie dan deze lelijke hack
 
   const today = new Date();
 
@@ -40,23 +41,18 @@ const clock = function () {
   const secondsPassed = today.getSeconds() / 60;
   const secondsDegrees = secondsPassed * 360;
   seconds.style.transform = `rotate(${secondsDegrees}deg)`;
+
   // breken minuten
   const minutesPassed = (secondsPassed + today.getMinutes()) / 60;
   const minuteDegrees = minutesPassed * 360;
   minutes.style.transform = `rotate(${minuteDegrees}deg)`;
+
   // bereken uren
   const hoursPassed = (minutesPassed + today.getHours()) / 12 + offset;
 
   const hourDegrees = hoursPassed * 360;
 
   hours.style.transform = `rotate(${hourDegrees}deg)`;
-};
+}
 
-setInterval(clock, 1000);
-
-// new york offset - 0 (-4)
-// london offset -0.3 (-1)
-// amsterdam offset -0.2 (+2)
-// moskou offset - 0.1 (+3)
-
-// tokyo offset -0.3 (+9)
+clock();
